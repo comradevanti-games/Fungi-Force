53a619de5edb3928df3655b29c34b78610dcb6f8
Ramon Brullo <kingvanti650@gmail.com>
2023-02-04 08:57:48 +0100

    Update MapGen.cs

--- Assets/Scripts/Runtime/MapGen.cs
+++ Assets/Scripts/Runtime/MapGen.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using UnityEngine;
 
 namespace TeamShrimp.GGJ23
@@ -10,15 +11,31 @@ namespace TeamShrimp.GGJ23
             new Map(ImmutableDictionary<Vector2Int, TileType>.Empty);
 
 
-        public static Map GenerateMap(GenerationParams genGenerationParams) =>
-            Empty;
+        private static IEnumerable<Vector2Int> PositionsInMapOfSize(int size)
+        {
+            for (var x = -(size - 1); x <= size - 1; x++)
+                yield return new Vector2Int(x, 0);
+        }
+
+        private static Map PlaceTileAt(
+            Map map, Vector2Int pos, TileType tile) =>
+            new Map(map.TilesByPosition.Add(pos, tile));
+
+
+        public static Map GenerateMap(GenerationParams genGenerationParams)
+        {
+            Map GenerateTile(Map map, Vector2Int pos) =>
+                PlaceTileAt(map, pos, genGenerationParams.TileType);
+
+            return PositionsInMapOfSize(genGenerationParams.Size)
+                .Aggregate(Empty, GenerateTile);
+        }
 
         public record GenerationParams(
-            int Seed,
             int Size,
             TileType TileType);
 
         public record Map(
-            IReadOnlyDictionary<Vector2Int, TileType> TilesByPosition);
+            IImmutableDictionary<Vector2Int, TileType> TilesByPosition);
     }
 }
