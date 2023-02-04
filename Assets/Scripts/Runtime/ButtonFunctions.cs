8c937673b1ca4bbab8fad773175fb8da0079183e
Xehnoh <54102548+Xehnoh@users.noreply.github.com>
2023-02-04 10:34:50 +0100

    feat: added StartMenu
    added a startmenu scene and a rudimentary design for it

---
+++ Assets/Scripts/Runtime/ButtonFunctions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamShrimp.GGJ23
+{
+    public class ButtonFunctions : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
