fb66741d99f8c68dd9807b577abd5e5edf4719eb
Ramon Brullo <kingvanti650@gmail.com>
2023-02-04 09:29:38 +0100

    Add test forest tile

--- Assets/Scripts/Runtime/TileType.cs
+++ Assets/Scripts/Runtime/TileType.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace TeamShrimp.GGJ23
 {
     [CreateAssetMenu(menuName = "GGJ23/Tile-type")]
     public class TileType : ScriptableObject
     {
+
+        [SerializeField] private Tile[] variants;
+
+
+        public IReadOnlyCollection<Tile> Variants => variants;
+
     }
 }
