fad292638c467b471c4215b7005fa0dae5697f9b
Ferris Schwarz <fbartak@student.tgm.ac.at>
2023-02-04 17:26:20 +0100

    Added "ALL" Command type, which can be subscribed to to listen to all incoming BaseCommands

---
+++ Assets/Scripts/Runtime/Networking/AutomaticallyInitializeNetworkManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace TeamShrimp.GGJ23.Networking
+{
+    public class AutomaticallyInitializeNetworkManager : MonoBehaviour
+    {
+        public bool isServer;
+        public void Start()
+        {
+            GetComponent<NetworkManager>().Init(isServer);
+        }
+    }
+}
