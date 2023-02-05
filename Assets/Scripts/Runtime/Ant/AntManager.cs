3caeb67868b4e0fb7fab80278a49611fcd892ba2
Ferris Schwarz <fbartak@student.tgm.ac.at>
2023-02-05 15:43:21 +0100

    Ant changes, bait

--- Assets/Scripts/Runtime/Ant/AntManager.cs
+++ Assets/Scripts/Runtime/Ant/AntManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using TeamShrimp.GGJ23.Runtime.Util;
 using UnityEngine;
 
 namespace TeamShrimp.GGJ23
@@ -11,8 +13,14 @@ namespace TeamShrimp.GGJ23
         private MushroomManager mushroomManager;
         private MapKeeper mapKeeper;
         public Grid grid;
+
+        public List<Vector3Int> baitPositions = new List<Vector3Int>();
+
+        public static AntManager instance;
+        
         public void Start()
         {
+            instance = this;
             ants.Add(GameObject.Instantiate(antPrefab));
             mapKeeper = GetComponent<MapKeeper>();
             mushroomManager = GetComponent<MushroomManager>();
@@ -21,14 +29,46 @@ namespace TeamShrimp.GGJ23
                 ant.Initialize(Vector3Int.zero, AntBehaviour.OffsetRotation.DOWN_RIGHT,mapKeeper, mushroomManager, grid);
                 Debug.Log("INITING ANT");
             }
+            //InvokeRepeating("Turn", 0.5f, 0.5f);
         }
 
         public void Turn()
         {
             foreach (var ant in ants)
             {
-                ant.Turn();
+                try
+                {
+                    ant.Turn();
+                }
+                catch
+                {
+                    Debug.Log("CANT DO ANT TURN YET");
+                }
             }
+        }
+
+        public Vector3Int? CheckForBaitInRange(Vector3Int pos, List<Vector3Int> takenBaits)
+        {
+            var v = baitPositions.Where(bait => bait.CubeDistance(pos) <= 3 && bait.CubeDistance(pos) > 0).ToList();
+            v.RemoveAll(match => takenBaits.Contains(match));
+            v.Sort((i, i2) => i.CubeDistance(pos).CompareTo(i2.CubeDistance(pos)));
+            if (v.Count == 0)
+                return null;
+            return v[0];
+        }
+
+
+        public void OnDrawGizmosSelected()
+        {
+            foreach (var bait in baitPositions)
+            {
+                Gizmos.DrawCube(grid.CellToWorld(bait.CubeToOffset()),Vector3.one/3f);
+            }
+        }
+
+        public List<Vector3Int> GetRoute(Vector3Int cubestart, Vector3Int cubeend)
+        { 
+            return mapKeeper.GetLerpPathCubed(cubestart.CubeToOffset(), cubeend.CubeToOffset());
         }
     }
 }
